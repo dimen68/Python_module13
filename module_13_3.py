9ea230359ef550cdf137efb3d6d087cbbc7548b2
bochar68 <bochar.dg@gmail.com>
2025-01-15 14:49:35 +0300

    написать простейшего телеграм-бота, используя асинхронные функции

---
+++ module_13_3.py
@@ -0,0 +1,24 @@
+# Задача "Он мне ответил!"
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '*************************************'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('В бот отправлено сообщение: Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def other_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    print('В бот отправлено сообщение: Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
