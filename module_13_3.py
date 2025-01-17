2ebca2a443d57e2b5775da86d02f833fb3fd67d2
bochar68 <bochar.dg@gmail.com>
2025-01-17 19:15:15 +0300

    убраны сервисные сообщения в консоль

--- module_13_3.py
+++ module_13_3.py
@@ -12,12 +12,12 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(commands=['start'])
 async def start(message):
     await message.answer('Привет! Я бот помогающий твоему здоровью.')
-    print('В бот отправлено сообщение: Привет! Я бот помогающий твоему здоровью.')
+
 
 @dp.message_handler()
 async def other_message(message):
     await message.answer('Введите команду /start, чтобы начать общение.')
-    print('В бот отправлено сообщение: Введите команду /start, чтобы начать общение.')
+
 
 
 if __name__ == '__main__':
