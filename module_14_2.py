fbe39c1143d3e001aea619946096229c1fc106f8
bochar68 <bochar.dg@gmail.com>
2025-01-25 13:24:11 +0300

    научится использовать функции внутри запросов языка SQL и использовать их в решении задачи

---
+++ module_14_2.py
@@ -0,0 +1,51 @@
+# Задача "Средний баланс пользователя"
+
+import sqlite3
+
+connection = sqlite3.connect('not_telegram1.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+               id INTEGER PRIMARY KEY,
+               username TEXT NOT NULL,
+               email TEXT NOT NULL,
+               age INTEGER,
+               balance INTEGER NOT NULL
+               )'''
+               )
+connection.commit()
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ? ,?)',
+                   (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
+connection.commit()
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, f'User{i}'))
+connection.commit()
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE username=?', (f'User{i}',))
+connection.commit()
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', ('60',))
+users_filt = cursor.fetchall()
+# for user in users_filt:
+#    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+# connection.close()
+
+# Новый код
+
+cursor.execute('DELETE FROM Users WHERE id=?', ('6',))
+connection.commit()
+
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_users = cursor.fetchone()[0]
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+all_balances = cursor.fetchone()[0]
+
+print(all_balances / total_users)
+connection.close()
