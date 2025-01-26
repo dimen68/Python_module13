5c0b7ea2df627457eef157da65191e6538218de0
bochar68 <bochar.dg@gmail.com>
2025-01-26 21:18:05 +0300

    подготовить Telegram-бота для взаимодействия с базой данных

---
+++ module_14_3.py
@@ -0,0 +1,115 @@
+# Задача "Витамины для всех!"
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+api = '7390097552:AAEu-Fp8_xpNl-pTd7HuU9tkgq1tt6Yizso'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb_start = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+kb_start.add(KeyboardButton('Рассчитать'), KeyboardButton('Информация'))
+kb_start.add(KeyboardButton('Купить'))
+
+kb_calories = InlineKeyboardMarkup()
+kb_calories.add(
+    InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories'),
+    InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+)
+
+kb_products = InlineKeyboardMarkup()
+kb_products.add(
+    InlineKeyboardButton(text='Витамин А', callback_data='product_buying'),
+    InlineKeyboardButton(text='Витамин В', callback_data='product_buying'),
+    InlineKeyboardButton(text='Витамин С', callback_data='product_buying'),
+    InlineKeyboardButton(text='Витамин D', callback_data='product_buying'),
+)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью!', reply_markup=kb_start)
+
+
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer(
+        'Информация о боте: Это бот, который помогает рассчитать количество необходимых калорий на основе данных пользователя',
+        reply_markup=kb_start)
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=kb_calories)
+
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    for i in range(1, 5):
+        await message.answer(f'Название: Product{i} | Описание: описание {i} | Цена: {i * 100}')
+        photo = f'{i}.jpg'
+        with open(photo, 'rb') as img:
+            await message.answer_photo(img)
+    await message.answer('Выберите продукт для покупки: ', reply_markup=kb_products)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer(
+        '10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+    await call.answer()
+    await call.message.answer('Выберите опцию:', reply_markup=kb_calories)
+
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст: ')
+    await call.answer()
+    await UserState.age.set()
+
+
+@dp.message_handler()
+async def other_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост: ')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес: ')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    colories = 5 * int(data['age']) + 6.25 * int(data['growth']) + 10 * int(data['weight']) + 5
+    await message.answer(f'Ваша норма калорий {colories}')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
